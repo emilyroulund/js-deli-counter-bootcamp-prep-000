a68c86fc49f488ad31d744d98c3e3e0fd6418d36
merry-lab-3791 <emilyroulund@gmail.com>
2019-03-14 20:16:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,5 +27,5 @@ function currentLine(line){
     return "The line is currently empty.";
   }
   else
-  return ("The line is currently:" + numberedLine);
+    return ("The line is currently:" + numberedLine);
 }
